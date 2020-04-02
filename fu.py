da487b9c2b397f2cd557ae42afa73a30eba9c840
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-02 14:57:30 -0400

    Create fu.py

---
+++ fu.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#Employer (mostly)endless loop
+import random
+import time
+
+fuckyou = random.randrange(1000000)
+
+answer = "Fuck you."
+while (fuckyou != 1):
+	print("Employer: We need you to do 'X'.")
+	print("Me: Ok, I need 'Y' to do that.")
+	print("Employer: ", answer)
+
+print("Employer: we need you to do 'X'.")
+print("Me: Ok, I need 'Y' to do that.")
+print("Employer: Sure, we can give you that.")
+print("Me: Thanks, I'll get working on X!")
+time.sleep(1)
+print("Sometime later...")
+time.sleep(1)
+print("Me: Jobs done!")

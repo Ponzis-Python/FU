fc7c376a5fcf26a71734f4e570fe07537c00e3fb
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-02 16:13:22 -0400

    Update fu.py

--- fu.py
+++ fu.py
@@ -8,14 +8,14 @@ fuckyou = random.randrange(1000000)
 answer = "Fuck you."
 while (fuckyou != 1):
 	print("Employer: We need you to do 'X'.")
-	print("Me: Ok, I need 'Y' to do that.")
+	print("Employee: Ok, I need 'Y' to do that.")
 	print("Employer: ", answer)
 
 print("Employer: we need you to do 'X'.")
-print("Me: Ok, I need 'Y' to do that.")
+print("Employee: Ok, I need 'Y' to do that.")
 print("Employer: Sure, we can give you that.")
-print("Me: Thanks, I'll get working on X!")
+print("Employee: Thanks, I'll get working on X!")
 time.sleep(1)
 print("Sometime later...")
 time.sleep(1)
-print("Me: Jobs done!")
+print("Employee: Jobs done!")
